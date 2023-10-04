3cd17331df55c4fb0868606e3d0985df703adf65
4Noyis <berenalpb@outlook.com>
2023-10-04 13:21:49 +0300

    commit

--- array-struct_example.sol
+++ array-struct_example.sol
@@ -4,4 +4,19 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract exampleContract{
 
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // dynmaic array
+    Person[] public listOfPeople;
+
+    function addPerson(uint256 _favoriteNumber, string memory _name) public{
+        Person memory newPerson= Person(_favoriteNumber, _name);
+        listOfPeople.push(newPerson);
+        // samething
+        listOfPeople.push(Person(_favoriteNumber, _name));
+    
+    }
 }
